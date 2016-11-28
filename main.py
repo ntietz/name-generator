29312ab87b25c3be4b199d5f2427ea1025fa7707
Nicholas Tietz <ntietz@gmail.com>
2016-11-28 15:45:06 -0500

    Really hacky name generation.

---
+++ main.py
@@ -0,0 +1,9 @@
+from namegen.namegen import *
+
+words = ["gigantic", "scalable", "software", "profitable"]
+
+print "Generating names using the entered words (%s)..." % (", ".join(words))
+
+combine_words(words, 30)
+
+
--- namegen/namegen.py
+++ namegen/namegen.py
@@ -1,13 +1,26 @@
 import pyphen
+import random
 
-def combine_words(words, lang='en_US'):
+def combine_words(words, num_results=10, lang='en_US'):
   if lang not in pyphen.LANGUAGES:
     print 'Language %s not found!' % lang
     return []
 
   d = pyphen.Pyphen(lang=lang)
 
+  bags = []
+
   for word in words:
     parts = d.inserted(word).split('-')
-    print parts
+    bags.append(parts)
+
+  for x in range(0, num_results):
+    num_samples = random.randint(2, len(bags)-1)
+    pick_from = random.sample(bags, num_samples)
+
+    name = ''
+    for bag in pick_from:
+      name += bag[random.randint(0,len(bag)-1)]
+
+    print name
 
